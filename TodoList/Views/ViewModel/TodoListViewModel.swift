301f83503ed49aacbeaf73bdaea5fe72079748c1
Hayden Chan <hchan@lcs.on.ca>
2024-05-24 15:00:49 -0400

    Added the ability to search and filter the list of to-do items.

--- TodoList/Views/ViewModel/TodoListViewModel.swift
+++ TodoList/Views/ViewModel/TodoListViewModel.swift
@@ -9,6 +9,36 @@ import Foundation
 
 @Observable
 class TodoListViewModel {
+    func filterTodos(on searchTerm: String) async throws {
+
+        if searchTerm.isEmpty {
+
+            // Get all the to-dos
+            Task {
+                try await getTodos()
+            }
+
+        } else {
+
+            // Get a filtered list of to-dos
+            do {
+                let results: [TodoItem] = try await supabase
+                    .from("todos")
+                    .select()
+                    .ilike("title", pattern: "%\(searchTerm)%")
+                    .order("id", ascending: true)
+                    .execute()
+                    .value
+
+                self.todos = results
+
+            } catch {
+                debugPrint(error)
+            }
+
+        }
+
+    }
     
     // MARK: Stored properties
     // The list of to-do items
